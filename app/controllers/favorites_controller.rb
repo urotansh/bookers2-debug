48e4d5b8ae007740bcdda589a986d50170203800
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-18 18:39:41 +0000

    [Add] 投稿一覧画面へいいね機能(create/destroy)を追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,15 @@
 class FavoritesController < ApplicationController
+  
+  def create
+    favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
+    favorite.save
+    redirect_to request.referer
+  end
+  
+  def destroy
+    favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
+    favorite.destroy
+    redirect_to request.referer
+  end
+  
 end
