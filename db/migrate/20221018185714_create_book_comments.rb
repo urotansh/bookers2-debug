b2f2fc483359a566a1fc939a31bf2f51a150a780
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-18 19:03:26 +0000

    [Add] book_commentsテーブルを作成(rails db:migrate)

--- db/migrate/20221018185714_create_book_comments.rb
+++ db/migrate/20221018185714_create_book_comments.rb
@@ -1,7 +1,9 @@
 class CreateBookComments < ActiveRecord::Migration[6.1]
   def change
     create_table :book_comments do |t|
-
+      t.text :comment
+      t.integer :user_id
+      t.integer :book_id
       t.timestamps
     end
   end
