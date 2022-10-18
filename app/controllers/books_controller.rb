09d2b63837b619b75a1c86656919ff9db68aca00
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-18 06:55:35 +0000

    Booksコントローラのindex/showアクションへ空インスタンスを定義&renderで参照

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < ApplicationController
 
   def show
+    @book_new = Book.new
     @book = Book.find(params[:id])
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
