d56793d412d14cc0cf90720624b2dd477ecdcac8
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-21 17:05:34 +0000

    [Add] Relationshipsコントローラ向けのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
   
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
