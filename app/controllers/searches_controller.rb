85bdaa9bbba6379d6a278123892d335e7aebfc3e
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-22 18:22:44 +0000

    [Add] ユーザ検索機能 完全一致/前方一致/後方一致/部分一致の検索条件を追加

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,9 +2,25 @@ class SearchesController < ApplicationController
   
   def search
     @keyword = params[:name]
+    search = params[:search]
     
     if @keyword.present?
-      @users = User.where('name LIKE ?', "%#{params[:name]}%")
+      # 完全一致検索
+      if search == "perfect_match"
+        @users = User.where(name: @keyword)
+      # 前方一致検索
+      elsif search == "forward_match"
+        @users = User.where('name LIKE ?', "#{@keyword}%")
+      # 後方一致検索
+      elsif search == "backward_match"
+        @users = User.where('name LIKE ?', "%#{@keyword}")
+      # 部分一致検索
+      elsif search == "partial_match"
+        @users = User.where('name LIKE ?', "%#{@keyword}%")
+      # それ以外
+      else
+        @users = User.none
+      end
     else
       @users = User.none
     end
