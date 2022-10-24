8387a2447f6c07173fcea48ccdf428e11ff85289
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-24 07:56:23 +0000

    [Update] いいね機能(books/index,books/show)の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
   
   def create
+    @book = Book.find(params[:book_id])
     favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
     favorite.save
-    redirect_to request.referer
   end
   
   def destroy
+    @book = Book.find(params[:book_id])
     favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
     favorite.destroy
-    redirect_to request.referer
   end
   
 end
